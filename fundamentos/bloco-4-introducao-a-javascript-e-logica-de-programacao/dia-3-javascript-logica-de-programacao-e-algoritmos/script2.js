9281a89375fe05bf2f7b9f53191b08be2b07dbfd
willia de OLiveira  Marques <willianmarques_fsa@hotmail.com>
2021-08-30 15:38:57 -0300

    Esta bom por hoje

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script2.js
@@ -0,0 +1,15 @@
+n = 5
+let ast = "*";
+  for(let ii=0; ii<5;ii+=1){
+    console.log("*"+ ast.repeat(ii))
+        
+}
+
+/*
+
+*
+**
+***
+****
+*****
+*/
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scrtpt3.js
@@ -0,0 +1,18 @@
+n = 5
+let ast = "*";
+let vac = " "
+
+    for(let ii=0; ii<=5;ii+=1){
+    console.log(vac.repeat(5-ii)+ ast.repeat(ii))
+    //document.write('saida');
+    
+}
+
+/*
+
+*
+**
+***
+****
+*****
+*/
