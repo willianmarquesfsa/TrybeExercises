a3c6cb65fd9460f1efb18e1ca585a30b1e68a4f4
Willian Marques <52426696+willianmarquesfsa@users.noreply.github.com>
2021-10-27 22:28:42 -0300

    Exercicio

---
+++ react10.1/src/ComCria.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function ComCria() {
+  return (
+   
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+  );
+}
+
+export default ComCria;
